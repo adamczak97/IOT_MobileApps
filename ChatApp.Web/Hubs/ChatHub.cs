a87dd435e5ca3e187dcbdb0a8fa6f16b63e0865c
adamczakl <aadamczak@edu.cdv.pl>
2021-07-05 21:21:51 +0200

    Add project files.

---
+++ ChatApp.Web/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChatApp.DTO.Models;
+using ChatApp.DTO;
+
+namespace ChatApp.Web.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(UserChatMessage message)
+        {
+            message.TimeStamp = System.DateTime.Now;
+            await Clients.All.SendAsync(Consts.RECEIVE_MESSAGE, message);
+            
+        }
+    }
+}
---
+++ ChatApp.Web/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    var txtMessage = $('#txtMessage')
+    var btnSend = $('#btnSend')
+    var listMessages = $('#listMessages')
+    var connection = new signalR.HubConnectionBuilder().withUrl('/chathub').build();
+    var userName = $('#userName').val();
+
+    $(btnSend).click(function () {
+        var userMessage = $(txtMessage).val();
+
+        connection.invoke('SendMessage', {
+            userName: userName,
+            message: userMessage
+        }).catch(function (error) {
+            alert("Can't send the message");
+            console(error);
+        })
+        $(txtMessage).val('');
+    });
+
+    connection.start().then(function () {
+        console.log('Connected to the signalr hub');
+        $(btnSend).removeAttr('disabled');
+    })
+
+    connection.on('ReceiveMessage', function (obj) {
+        $(listMessages).prepend('<li>'
+            + '<span class="badge bg-primary">'+ obj.timeStampString + '</span>'
+            + '<span class="badge bg-info text-dark font-weight-bold">' + obj.username
+            + '</span><br/>' + obj.message
+            + '</li>' )
+    })
+})();
